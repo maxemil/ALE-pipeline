8bb45191a91c68d606f12dd9d0d02fe855ac1134
maxemil <max-emil.schon@icm.uu.se>
2019-09-24 15:28:04 +0200

    Improve name replacement script

--- templates/replace_names_tree.py
+++ templates/replace_names_tree.py
@@ -7,22 +7,28 @@ def replace(string, patterns):
         string = string.replace(p[0], p[1])
     return string
 
-shutil.copyfile("$bootstrap", "${bootstrap.simpleName}.clean.ufboot")
+def main(bootstrap, prefix, separator):
+    inputfile = "map_genes.txt"
+    replacementfile = "{}.clean.ufboot".format(prefix)
 
-inputfile = "map_genes.txt"
-replacementfile = "${bootstrap.simpleName}.clean.ufboot"
+    shutil.copyfile(bootstrap, replacementfile)
 
-patterns = set()
-for line in open(inputfile):
-    line = line.strip().split('\t')
-    patterns.add((line[0], line[1]))
+    patterns = set()
+    for line in open(inputfile):
+        line = line.strip().split('\t')
+        patterns.add((line[0], line[1]))
 
-content = open(replacementfile).read()
-content = replace(content, patterns)
+    content = open(replacementfile).read()
+    content = replace(content, patterns)
 
-content = content.replace("_","")
-content = content.replace("-","")
-content = content.replace("$params.separator","_")
+    content = content.replace("_","")
+    content = content.replace("-","")
+    content = content.replace(separator,"_")
+    content = content.replace(".","")
 
-with open(replacementfile, 'w') as outhandle:
-    outhandle.write(content)
+    with open(replacementfile, 'w') as outhandle:
+        outhandle.write(content)
+
+
+if __name__ == '__main__':
+    main("$bootstrap", "${bootstrap.simpleName}", "$params.separator")
